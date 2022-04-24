737a7133dc305a08a5384006aa836d1724fc8def
Arllain <arllain@gmail.com>
2022-04-24 12:32:55 -0300

    calculadora backend

---
+++ src/test/java/com/arllain/calculadorabackend/CalculadoraBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arllain.calculadorabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculadoraBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
